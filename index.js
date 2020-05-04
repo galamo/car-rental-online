9d1574e140fb1bbea4112493935b0cba6d1cf480
Gal Amouyal <gal.amouyal@ibm.com>
2020-05-04 20:30:47 +0300

    drawrow

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const cars = [];
 // attaching the event to the element DOM
 // document.getElementById("addNewCar").onclick;
 
+// event on click - trigger for car creation / drawing
 function addCar() {
   //validations, lp regex, prevent null and undefined
   cars.push({
@@ -22,7 +23,9 @@ function addCar() {
   // draw the array into the table
 }
 
+// general function for drawing an array of data into the DOM
 function draw(arrayOfData) {
+  if (!Array.isArray(arrayOfData)) return;
   // clear table
   clearTable();
   for (let index = 0; index < arrayOfData.length; index++) {
@@ -31,6 +34,23 @@ function draw(arrayOfData) {
   // draw into the table
 }
 
+// general function for row creation
+function drawRow(model) {
+  const carTR = getCarTR(model);
+  if (!carTR) return;
+  CAR_FORM.carsData.appendChild(carTR);
+}
+
+function getCarTR(car) {
+  // car.lp
+  // car.price
+  // car.model
+  const { lp, price, model } = car;
+  const tableRow = document.createElement("tr")
+
+}
+
+//clear the InnerHTML from the table
 function clearTable() {
   CAR_FORM.carsData.innerHTML = "";
 }
